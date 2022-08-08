807dd8903c8cda9cbd614f8c5858bb2e8797deca
Jon-Collin Chinje <j.chinje@gmail.com>
2022-08-08 16:46:28 -0400

    completed layout for gameboard

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -21,7 +21,9 @@ function App() {
   return (
     <div className="App">
       <Nav />
-      <GameBoard />
+      <GameBoard 
+        players={players}
+      />
     </div>
   );
 }
--- client/src/components/GameBoard/GameBoard.js
+++ client/src/components/GameBoard/GameBoard.js
@@ -1,9 +1,24 @@
 import React from 'react'
 import './GameBoard.css'
 
-function GameBoard() {
+function GameBoard({ players }) {
+
+  let player1 = players[0]
+  let player2 = players[1]
+  
   return (
-    <div>GameBoard</div>
+    <div id="game-holder">
+      <div id="player-one" className="player-half">
+        <div className="player-name">{player1.name}</div>
+        <div className="player-card">CARD</div>
+        <div className="card-count">0</div>
+      </div>
+      <div id="player-two" className="player-half">
+        <div className="player-name">{player2.name}</div>
+        <div className="player-card">CARD</div>
+        <div className="card-count">0</div>
+      </div>
+    </div>
   )
 }
 
