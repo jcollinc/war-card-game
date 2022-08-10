84964bbe8e71357556730c9cdf3134bcf460ce37
Jon-Collin Chinje <j.chinje@gmail.com>
2022-08-10 13:08:06 -0400

    deploy

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   
   resources :players, :only => [:index, :update]
+  
+  get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
 
 end
