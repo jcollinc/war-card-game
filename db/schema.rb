6ff3a37d532e7607e8879bab1dc76512f02f72c6
Jon-Collin Chinje <j.chinje@gmail.com>
2022-08-10 12:47:25 -0400

    switching to psql

--- db/schema.rb
+++ db/schema.rb
@@ -14,7 +14,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_07_231801) do
   create_table "players", force: :cascade do |t|
     t.string "name"
     t.integer "games_won"
-    t.integer "rounds_won"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
